d86beadeea0495bfc69a7f15cfc79aaf4e919871
Viktor <slavik00757@gmail.com>
2022-09-30 10:56:08 +0200

    reszponziv a biography uj header hatterkep kis featurek

--- hattervaltas.js
+++ hattervaltas.js
@@ -3,6 +3,7 @@ function changeBg() {
     const images = [
         'url("kepek/wallpaperflare.com_wallpaper.jpg")',
         'url("kepek/wallpaper 2.jpg")',
+        'url("kepek/wallpaper3.jpg")',
     ]
     const header = document.querySelector('header');
     const bg = images[Math.floor(Math.random() * images.length)];
